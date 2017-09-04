ca4d179fdb503a5c8d80113dbe03bc6eba9f03ac
Faggioni <miguelfaggioni@gmail.com>
2017-09-03 23:01:37 -0400

    Include Problem

--- caag-forms.php
+++ caag-forms.php
@@ -11,13 +11,16 @@ Version: 0.2
 Author URI: https://www.caagsoftware.com/
 */
 
+/*
+ * Global Attributtes
+ */
 define('CAAG_FORMS_VERSION','0.1');
 define('CAAG_FORMS_ROOT', __FILE__);
 define('CAAG_CUSTOM_POST_TYPE','caag_form');
 define('CAAG_FORMS_SLUG','caag-forms');
 
 /*
- * Special Attributtes
+ * Special Plugin Attributtes
  */
 define('CAAG_FORMS_LINK','caag_form_link');
 define('CAAG_FORMS_ID','caag_form_id');
@@ -25,11 +28,14 @@ define('CAAG_FORMS_SHORTCODE','caag_shortcode');
 define('CAAG_FORMS_TENANT_TOKEN','caag_tenant_token');
 define('CAAG_FORMS_USER_TOKEN','caag_user_token');
 
+
 require_once 'includes/setup.php';
 require_once 'includes/utils.php';
+require_once 'includes/options.php';
 require_once 'includes/metaboxes.php';
 require_once 'shortcodes/shortcodes.php';
-require_once 'includes/options.php';
+
+
 /*
  * Install Plugin
  */
@@ -49,4 +55,5 @@ function caag_forms_deactivate()
 	//Nothing To Do
 }
 register_deactivation_hook(__FILE__,'caag_forms_deactivate');
+
 
--- includes/options.php
+++ includes/options.php
@@ -1,23 +1,40 @@
 <?php
 
+/*
+ * Hooking Function to Create Setting Submenu
+ */
+
+add_action('admin_menu', 'caag_forms_setting_menu');
+function caag_forms_setting_menu()
+{
+	add_options_page(
+		'Caag Forms Settings',
+		'Caag Forms',
+		'manage_options',
+		'caag',
+		'caag_form_menu_setting_html'
+	);
+}
+
 function caag_form_menu_setting_html()
 {
 	$settings = get_caag_user_settings();
 	?>
-		<div class="wrap">
-			<div id="titlewrap">
-				<label class="screen-reader-text" id="title-prompt-text" for="title">Enter title here</label>
-				<input type="text" name="post_title" size="30" value="<?php echo $settings[CAAG_FORMS_TENANT_TOKEN]; ?>" id="title" spellcheck="true" autocomplete="off">
-			</div>
-			<div id="titlewrap">
-				<label class="screen-reader-text" id="title-prompt-text" for="title">Enter title here</label>
-				<input type="text" name="post_title" size="30" value="Registro 2" id="title" spellcheck="true" autocomplete="off">
-			</div>
-			<input type="submit" name="publish" id="publish" class="button button-primary button-large" value="Publish">
+	<div class="wrap">
+		<div id="titlewrap">
+			<label class="screen-reader-text" id="title-prompt-text" for="title">Enter title here</label>
+			<input type="text" name="post_title" size="30" value="<?php echo $settings[CAAG_FORMS_TENANT_TOKEN]; ?>" id="title" spellcheck="true" autocomplete="off">
 		</div>
+		<div id="titlewrap">
+			<label class="screen-reader-text" id="title-prompt-text" for="title">Enter title here</label>
+			<input type="text" name="post_title" size="30" value="Registro 2" id="title" spellcheck="true" autocomplete="off">
+		</div>
+		<input type="submit" name="publish" id="publish" class="button button-primary button-large" value="Publish">
+	</div>
 	<?php
-}
 
+}
+add_action('caag_form_menu_setting_html','caag_form_menu_setting_html');
 
 function add_caag_forms_setting_options()
 {
@@ -35,15 +52,3 @@ function get_caag_user_settings()
 	return $settings;
 }
 add_action('get_caag_user_settings','get_caag_user_settings');
-
-add_action('admin_menu', 'caag_forms_setting_menu');
-function caag_forms_setting_menu()
-{
-	add_options_page(
-		'Caag Forms Settings',
-		'Caag Forms',
-		'manage_options',
-		'caag',
-		'caag_form_menu_setting_html'
-	);
-}
