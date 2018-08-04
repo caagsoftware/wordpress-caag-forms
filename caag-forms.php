db832329d3e4ddfb951672a723ed4e3f9030d66a
Faggioni <miguelfaggioni@gmail.com>
2018-08-04 11:38:10 -0400

    new version

--- caag-forms.php
+++ caag-forms.php
@@ -7,7 +7,7 @@
 Plugin Name: Caag Forms
 Description: Use this plugin to easily add Caag Software Forms
 Author: Caag Software
-Version: 1.2
+Version: 2.0.0
 Author URI: https://www.caagsoftware.com/
 */
 
@@ -17,7 +17,7 @@ Author URI: https://www.caagsoftware.com/
 $root = __DIR__;
 $folders = explode('/', $root);
 $folder = $folders[count($folders) - 1];
-define('CAAG_FORMS_VERSION','1.2');
+define('CAAG_FORMS_VERSION','2.0.0');
 define('CAAG_FORMS_ROOT', __DIR__);
 define('CAAG_FORMS_ROOT_FILE', __FILE__);
 define('CAAG_CUSTOM_POST_TYPE','caag_form');
